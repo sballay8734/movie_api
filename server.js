44a18aceb15dee6e61ac2c84ef5de4e8f0da8f1f
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2022-12-14 14:35:25 -0500

    passing to desktop

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 // built in modules
 import * as http from 'http';
+import * as url from 'url';
+import * as fs from 'fs';
 
 
-http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-type': 'text/plain' }); // 200 = 'OK'
-  response.end('Hello Node!\n');
-}).listen("8080")
+// http.createServer((request, response) => {
+//   response.writeHead(200, { 'Content-type': 'text/plain' }); // 200 = 'OK'
+//   response.end('Hello Node!\n');
+// }).listen("8080")
 
-console.log("Hello");
+// console.log("Hello");
 
+fs.readFile('file.txt', (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Data read: " + data);
+})
 
 
 
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,24 +1,5 @@
-let masterStatsList = [
-  {
-    manager: "shawnB",
-    pointsFor: 1648.94,
-    wins: 11,
-    losses: 3,
-    ties: 0,
-  },
-  {
-    manager: "steveS",
-    pointsFor: 1804.00,
-    wins: 8,
-    losses: 6,
-    ties: 0,
-  }
-]
-
-let masterList2 = [];
-
-masterStatsList.forEach(item => {
-  if (item.manager == "shawnB") {
-    console.log(item)
-  }
-});
+let addr = 'http://localhost:8080/default.html?year=2017&month=february'
+let q = url.parse(addr, true);
+console.log(q.host);
+console.log(q.pathname);
+console.log(q.search);
