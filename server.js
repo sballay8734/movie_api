55301e7cadf262aa2cb88c2af0d832b8a7ec5580
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2022-12-15 07:51:17 -0500

    pushing to laptop

--- server.js
+++ server.js
@@ -1,45 +1,45 @@
-import * as http from 'http';
-import * as url from 'url';
-import * as fs from 'fs';
-
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url); 
-const __dirname = path.dirname(__filename);
+const http = require('http'),
+      url = require('url'),
+      fs = require('fs');
 
 // Create Server
 http.createServer((request, response) => {
-  let addr = request.url,
-  q = url.parse(addr, true),
-  filePath = '';
+  let addr = request.url;
+  let q = url.parse(addr, true);
+  let filePath = '';
 
-  // Add requests to log file
+  // log requests to log.txt
   fs.appendFile('log.txt', `URL: ${addr}\nTimestamp: ${new Date()}\n\n`, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('Added to log.')
-    }
-  })
-
-  // check if pathname includes 'documentation'
-  if (q.pathname.includes('documentation')) {
-    filePath = (__dirname + '/documentation.html');
-  } else {
-    filePath = 'index.html'
-  }
-
-  // Read file & write data to page
-  fs.readFile(filePath, (err, data) => {
     if (err) {
       throw err;
     }
+  })
 
+  // check for pathname
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
+
+  // read file
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.log(err)
+    }
+
+    // tell browser content type
     response.writeHead(200, { 'Content-Type': 'text/html' });
+    // write data
     response.write(data);
     response.end();
 
-  });
+  })
 
 }).listen(8080)
+
+
+
+
+
+
