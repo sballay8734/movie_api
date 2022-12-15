fd41123c3d13d2b40c893829bda80dd9f41bcb2d
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2022-12-14 20:24:08 -0500

    Ach2Task2V1

--- server.js
+++ server.js
@@ -1,66 +1,78 @@
-// built in modules
 import * as http from 'http';
 import * as url from 'url';
 import * as fs from 'fs';
-import * as url from 'url';
-import * as fs from 'fs';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename);
+
+
+// http.createServer((request, response) => {
+//   let addr = request.url,
+//   q = url.parse(addr, true),
+//   filePath = '';
+
+//   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log('Added to log.')
+//     }
+//   })
+
+//   if (q.pathname.includes('documentation')) {
+//     filePath = (__dirname + '/documentation.html');
+//   } else {
+//     filePath = 'index.html';
+//   }
+
+//   fs.readFile(filePath, (err, data) => {
+//     if (err) {
+//       throw err;
+//     }
+
+//     response.writeHead(200, { 'Content-Type': 'text/html' });
+//     response.write(data);
+//     response.end();
+
+//   })
+
+// }).listen(8080);
 
 
 http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-type': 'text/plain' }); // 200 = 'OK'
-  response.end('Hello Node!\n');
-}).listen("8080")
+  let addr = request.url,
+  q = url.parse(addr, true),
+  filePath = '';
 
-console.log("Hello");
+  // Add requests to log file
+  fs.appendFile('log.txt', `URL: ${addr}\nTimestamp: ${new Date()}\n\n`, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Added to log.')
+    }
+  })
 
-fs.readFile('file.txt', (err, data) => {
-  if (err) {
-    throw err;
+  // check if pathname includes 'documentation'
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html'
   }
-  console.log("Data read: " + data);
-})
 
+  // Read file & write data to page
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      throw err;
+    }
 
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
 
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}).listen(8080)
--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-function bob(num) {
-  return num * 2;
-}
-
-const bob2 = (num) => num * 2;
-
-console.log(bob(7));
-console.log(bob2(8))
