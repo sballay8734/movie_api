c2eb235a5bae23b6e82d7c7ba4c68153058e6ed2
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2022-12-14 20:25:01 -0500

    Ach2Task2V1

--- server.js
+++ server.js
@@ -8,40 +8,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url); 
 const __dirname = path.dirname(__filename);
 
-
-// http.createServer((request, response) => {
-//   let addr = request.url,
-//   q = url.parse(addr, true),
-//   filePath = '';
-
-//   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log('Added to log.')
-//     }
-//   })
-
-//   if (q.pathname.includes('documentation')) {
-//     filePath = (__dirname + '/documentation.html');
-//   } else {
-//     filePath = 'index.html';
-//   }
-
-//   fs.readFile(filePath, (err, data) => {
-//     if (err) {
-//       throw err;
-//     }
-
-//     response.writeHead(200, { 'Content-Type': 'text/html' });
-//     response.write(data);
-//     response.end();
-
-//   })
-
-// }).listen(8080);
-
-
+// Create Server
 http.createServer((request, response) => {
   let addr = request.url,
   q = url.parse(addr, true),
