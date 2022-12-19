cb0bdfa2207632e6e1787a50a390a43d329a383b
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2022-12-19 07:55:43 -0500

    passing

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+
+let topBooks = [
+  {
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien'
+  },
+  {
+    title: 'Twilight',
+    author: 'Stephanie Meyer'
+  },
+];
