77a72dc584bacbab7d3da8ef31b61dbe98764120
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-01-04 14:09:01 -0500

    Passing to desktop

---
+++ forCranfieldAppCalendar.js
@@ -0,0 +1,16 @@
+let calendarObject = {
+  jan: {
+    first: {
+      track1: true,
+      track2: false,
+      track3: true,
+      track4: true
+    },
+    second: {},
+    third: {},
+    fourth: {},
+  },
+  feb: {},
+  march: {},
+  april: {},
+}
