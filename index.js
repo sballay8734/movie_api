609c7d21aa1ea4826e4b42cc2479a792de7cabbb
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-01-07 13:14:21 -0500

    Ach2_Task5_V1

--- index.js
+++ index.js
@@ -60,10 +60,6 @@ let directors = [
   },
 ]
 
-// directors.forEach((director) => {
-//   console.log(`${director.name}: ${director.description}\n`)
-// })
-
 // Gets the list of data about ALL movies: WORKING
 app.get('/movies', (req, res) => {
   let moviesList = [];
@@ -128,64 +124,10 @@ app.delete('/users/:username/deregister', (req, res) => {
   res.send('Successful Endpoint: Deregister')
 });
 
-// ****************************************************************************
-// Adds data for a new student to our list of students.
-app.post('/students', (req, res) => {
-  let newStudent = req.body;
 
-  if (!newStudent.name) {
-    const message = 'Missing name in request body';
-    res.status(400).send(message);
-  } else {
-    newStudent.id = uuid.v4();
-    students.push(newStudent);
-    res.status(201).send(newStudent);
-  }
-});
 
-// Deletes a student from our list by ID
-app.delete('/students/:id', (req, res) => {
-  let student = students.find((student) => { return student.id === req.params.id });
 
-  if (student) {
-    students = students.filter((obj) => { return obj.id !== req.params.id });
-    res.status(201).send('Student ' + req.params.id + ' was deleted.');
-  }
-});
 
-// Update the "grade" of a student by student name/class name
-app.put('/students/:name/:class/:grade', (req, res) => {
-  let student = students.find((student) => { return student.name === req.params.name });
-
-  if (student) {
-    student.classes[req.params.class] = parseInt(req.params.grade);
-    res.status(201).send('Student ' + req.params.name + ' was assigned a grade of ' + req.params.grade + ' in ' + req.params.class);
-  } else {
-    res.status(404).send('Student with the name ' + req.params.name + ' was not found.');
-  }
-});
-
-// Gets the GPA of a student
-app.get('/students/:name/gpa', (req, res) => {
-  let student = students.find((student) => { return student.name === req.params.name });
-
-  if (student) {
-    let classesGrades = Object.values(student.classes); // Object.values() filters out object's keys and keeps the values that are returned as a new array
-    let sumOfGrades = 0;
-    classesGrades.forEach(grade => {
-      sumOfGrades = sumOfGrades + grade;
-    });
-
-    let gpa = sumOfGrades / classesGrades.length;
-    console.log(sumOfGrades);
-    console.log(classesGrades.length);
-    console.log(gpa);
-    res.status(201).send('' + gpa);
-    //res.status(201).send(gpa);
-  } else {
-    res.status(404).send('Student with the name ' + req.params.name + ' was not found.');
-  }
-});
 
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080');
