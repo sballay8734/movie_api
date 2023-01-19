63387f227988986259a7fd0962b4828498f8a211
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-01-19 07:47:07 -0500

    working through 2.9

---
+++ passport.js
@@ -0,0 +1,54 @@
+const passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy,
+  Models = require('./models.js'),
+  passportJWT = require('passport-jwt');
+
+
+let Users = Models.User,
+  JWTStrategy = passportJWT.Strategy,
+  ExtractJWT = passportJWT.ExtractJwt;
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+// Local Strategy **************************************************************
+passport.use(new LocalStrategy({
+  usernameField: 'username',
+  passwordField: 'password'
+},
+
+  (username, password, callback) => {
+    console.log(username + ' ' + password);
+
+    Users.findOne({ Username: username }, (error, user) => {
+      if (error) {
+        console.log(error);
+        return callback(error);
+      }
+
+      if (!user) {
+        console.log('incorrect username');
+        return callback(null, false, { message: 'Incorrect username or password' });
+      }
+
+      console.log('finished');
+      return callback(null, user);
+    });
+
+  }));
+
+// JWT Strategy ****************************************************************
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'YudE3ZYZdAbS4Bj5jkpaKR5KwOkrK8Gx'
+},
+
+  (jwtPayload, callback) => {
+    return Users.findByID(jwtPayload._id)
+      .then((user) => {
+        return callback(null, user);
+      })
+      .catch((error) => {
+        return callback(error)
+      });
+
+  }));
