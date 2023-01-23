7b46e2af92baa4b30598490c59a7f7cc66e55582
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-01-23 07:39:02 -0500

    working through 2.10

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ app.use(bodyParser.json());
 
 let auth = require('./auth')(app);
 
+// CORS Stuff
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    
+    return callback(null, true);
+  }
+}));
+
 const passport = require('passport');
 require('./passport');
 
