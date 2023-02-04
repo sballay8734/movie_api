0efcb59d168a29534421e2b18a41e0de6df5b0b7
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-02-04 16:54:19 -0500

    allow local host origin

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ let auth = require('./auth')(app);
 
 // CORS Stuff
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
