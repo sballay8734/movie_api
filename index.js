eece436a6eb757a8275ede864d864059a2356f56
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-02-11 11:29:38 -0500

    push to heroku

--- index.js
+++ index.js
@@ -91,6 +91,12 @@ app.get('/', (req, res) => {
 
 // });
 
+app.get('/login', (req, res) => {
+  Users.findOne({ username: req.params.username })
+    .then((user) => alert(user));
+  }
+)
+
 // Get all movies --------------------------------------------------------------
 app.get('/movies', passport.authenticate('jwt', { session: false }),
 (req, res) => {
