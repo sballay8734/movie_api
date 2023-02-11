e12e7b590d52f4da069824a7dcedee964ec1a823
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-02-11 12:07:48 -0500

    removed cors from auth

--- auth.js
+++ auth.js
@@ -14,21 +14,6 @@ let generateJWTToken = (user) => {
   });
 }
 
-const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-
-    return callback(null, true);
-  }
-}));
-
-
 /* POST login. */
 module.exports = (router) => {
   router.post('/login', (req, res) => {
